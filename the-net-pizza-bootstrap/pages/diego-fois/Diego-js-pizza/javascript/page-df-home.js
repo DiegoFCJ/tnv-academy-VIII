486de1c1918c1d1676e3857c78d3f24f9414425e
Diego <diegoeffe96@gmail.com>
2022-11-16 14:12:53 +0100

    drinks fixed, db fixed, choiches menu on landing not working

--- the-net-pizza-bootstrap/pages/diego-fois/Diego-js-pizza/javascript/page-df-home.js
+++ the-net-pizza-bootstrap/pages/diego-fois/Diego-js-pizza/javascript/page-df-home.js
@@ -1,8 +1,56 @@
-const buildCard = (item) => {
+/** build choices on landing page */
+
+const buildChoicesMenu = (menuItems, sectionName) => {
+    const section = document.getElementById(sectionName);
+    const sectionMenuItems = menuItems.filter((x) => x.category === sectionName);
+
+    for(let item of sectionMenuItems){
+        const cardWrapper = document.createElement('div');
+        cardWrapper.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-2');
+
+        const cardContent = buildChoicesCard(item);
+        cardWrapper.innerHTML = cardContent;
+        section.appendChild(cardWrapper);
+    }
+};
+
+const buildChoicesCard = (item) => {
+    return `
+        <a 
+        class="card"
+        href="${item.href}"
+        >
+            <img src="${item.imageUrl}" alt="${item.title}" style="height: 220px;">
+            <div class="card-img-overlay">
+                <h5 class="card-title text-bg-light fw-bold fs-3 text-center">${item.title}</h5>
+            </div>
+        </a>`;
+};
+
+
+
+
+
+/** build drinks */
+const buildDrinksMenu = (menuItems, sectionName) => {
+    const section = document.getElementById(sectionName);
+    const sectionMenuItems = menuItems.filter((x) => x.category === sectionName);
+
+    for(let item of sectionMenuItems){
+        const cardWrapper = document.createElement('div');
+        cardWrapper.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-2');
+
+        const cardContent = buildDrinksCard(item);
+        cardWrapper.innerHTML = cardContent;
+        section.appendChild(cardWrapper);
+    }
+};
+
+const buildDrinksCard = (item) => {
     return `
     <div class="card shadow">
     <img
-    src="h${item.imageUrl}"
+    src="${item.imageUrl}"
     class="card-img-top"
     alt="${item.title}"
     />
@@ -14,27 +62,18 @@ const buildCard = (item) => {
 };
 
 
-const buildMenu = (menuItems, sectionName) => {
-    const section = document.getElementById(sectionName);
-    const sectionMenuItems = menuItems.filter((x) => x.category === sectionName);
-
-    for(let item of sectionMenuItems){
-        const cardWrapper = document.createElement('div');
-        cardWrapper.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-2');
-
-        const cardContent = buildCard(item);
-        cardWrapper.innerHTML = cardContent;
-        section.appendChild(cardWrapper);
-    }
-};
-
-
+/** build page */
+/*
 const buildPage = (menuItems) => {
 
 }
 
 
-fetch('https://my-json-server.typicode.com/DiegoFCJ/demo_fs_json/drinkCards')
+/** fetching the server */
+
+fetch('https://my-json-server.typicode.com/DiegoFCJ/pizza_fake_db/pizzaDiegoDb')
 .then((res) => res.json())
-.then((res) => buildMenu(res, 'drinkCards'));
+.then((res) => buildDrinksMenu(res, 'drinkCards'))
+.then((res) => buildChoicesMenu(res, 'choicesOnLanding'))
+;
 
